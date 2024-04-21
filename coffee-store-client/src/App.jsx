1163a1b0cfdb5e8b8889fe35a5bd2b33412750d0
galibcse19 <mrgalib40@gmail.com>
2024-04-22 00:36:04 +0600

    make card for every coffee and show in site

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
  const coffees= useLoaderData();
   return (
     <> 
-      <h1 className='text-3xl text-center text-purple-500'>Coffee : {coffees.length}</h1>
-      {
+      <h1 className='text-3xl text-center text-purple-500 mt-4 '>Available Coffee : {coffees.length}</h1>
+      <div  className='grid md:grid-cols-2 gap-4 mt-10 md:mx-40 content-center'>
+        {
         coffees.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}> </CoffeeCard>)
       }
+      </div>
     </>
   )
 }
--- coffee-store-client/src/components/CoffeeCard.jsx
+++ coffee-store-client/src/components/CoffeeCard.jsx
@@ -1,18 +1,22 @@
- 
-
+  
 const CoffeeCard = ({coffee}) => {
     const {name,quantity,supplier,taste,category,details,photo} = coffee;
     return (
-        <div>
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure><img src={photo} alt="Movie"/></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>quantity: {quantity}</p>
-                    <p>{details}</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
-                    </div>
+        <div className="">
+            <div className="flex content-center justify-between bg-green-400 text-white">
+                <div className="">
+                    <figure><img className="w-40" src={photo} alt="Movie"/></figure>
+                </div>
+                <div className="pt-6 pl-10">
+                    <h2 className="card-title">Name: {name}</h2>
+                    <p className="my-4">Quantity: {quantity}</p>
+                    <p className="mb-4">Taste: {taste}</p>
+                    <p>Supplier: {supplier}</p>
+                </div>
+                <div className="join join-vertical mr-4 mt-4">
+                    <button className="btn join-item mb-4">VIEW</button>
+                    <button className="btn join-item mb-4">EDIT</button>
+                    <button className="btn join-item">DELETE</button>
                 </div>
             </div>
         </div>
